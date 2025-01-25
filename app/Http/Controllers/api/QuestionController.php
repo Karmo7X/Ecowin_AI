f109044c84241e2d371db5f1e74db579a1de7cda
Kareem Azam <maxmr343@gmail.com>
2025-01-25 23:21:33 +0200

    producut & category $ update Auth

--- app/Http/Controllers/api/QuestionController.php
+++ app/Http/Controllers/api/QuestionController.php
@@ -13,6 +13,24 @@ class QuestionController extends Controller
     public function index(Request $request)
     {
         $perpage = $request->input('perpage', 10);
+        $questions =Question::paginate($perpage);
+       return response()->json([
+           'message' => 'Questions returned successfully',
+           'status' => 200,
+           'data' => $questions->items(), // Return the paginated items
+           'meta' => [
+               'total' => $questions->total(),
+               'current_page' => $questions->currentPage(),
+               'last_page' => $questions->lastPage(),
+               'per_page' => $questions->perPage(),
+           ]
+       ],200);
+
+    }
+
+    public function question_search(Request $request)
+    {
+        $perpage = $request->input('perpage', 10);
         $search_term = $request->input('search');
         $questions = Question::where('question', 'like', '%' . $search_term . '%')
             ->orWhere('answer', 'like', '%' . $search_term . '%')
